7eea2ef90a9f91c05e23f7775bce321c1cb9d479
Trevor May <trevormay@Trevors-MacBook-Pro.local>
2019-09-30 12:51:40 -0600

    done with github

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Gallery from "./pages/Gallery";
+// import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import NoMatch from "./pages/NoMatch";
 import About from "./pages/About";
-import TopNav from "./components/TopNav";
+// import TopNav from "./components/TopNav";
 import Project from "./pages/Project";
 import Resume from "./pages/Resume";
 import $ from "jquery";
-import ReactGA from "react-ga";
+// import ReactGA from "react-ga";
 import "./style.css";
 
 
 // import { Container } from 'reactstrap';
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-
-  }
-
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
 
 
 
@@ -56,12 +27,12 @@ render() {
          
           <div>
             <Switch>
-              <Route exact path="/" component={Home} data={this.state.resumeData.home} />
-              <Route exact path="/About" component={About} data={this.state.resumeData.about} />
-              <Route exact path="/Contact" component={Contact} data={this.state.resumeData.contact} />
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/About" component={About} />
+              {/* <Route exact path="/Contact" component={Contact} data={this.state.contact} /> */}
               {/* <Route exact path="/Gallery" component={Gallery} data={this.state.resumeData.gallery} /> */}
-              <Route exact path ="/Project" component={Project} data={this.state.resumeData.project} />
-              <Route exact path="/Resume" component={Resume} data={this.state.resumeData.resume} />
+              <Route exact path ="/Project" component={Project}/>
+              <Route exact path="/Resume" component={Resume}  />
 
               <Route component= {NoMatch} />
             </Switch>
